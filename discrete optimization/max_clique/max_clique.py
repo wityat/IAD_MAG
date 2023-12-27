b2f3f81990703b93da2d1a2fb36f4d7929463907
wityat <wityat2013@yandex.ru>
2023-12-27 13:59:40 +0300

    add max_clique_greedy

--- discrete optimization/max_clique/max_clique.py
+++
@@ -1,8 +0,0 @@
-def graph_coloring_min_available_color_algorithm(graph):
-    graph.set_sorted_vertices_by_degree()
-
-    for vertex in graph.sorted_vertices:
-        min_color = graph.get_min_available_color(vertex)
-        graph.color_vertex(min_color, vertex)
-
-    return max(graph.colors_clusters)
